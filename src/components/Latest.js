3882c913971b52aafb6ea6214b48d1a6189e7411
100ngramjit <sangramjitdutta@gmail.com>
2022-02-23 13:26:37 +0530

    tested to see if latest page is working

--- src/components/Latest.js
+++ src/components/Latest.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './style.css';
 import {useState,useEffect} from 'react';
 import axios from 'axios';
+import Cards from './Cards'
 
 function Latest() {
   const [content,setContent]=useState([]);
 
   const fetchLatest=async()=>{
     const {data}=await axios.get("https://api.themoviedb.org/3/movie/upcoming?api_key=2f79f363fb4398298fea2fddf5996e0d&language=en-US&page=1");
-    console.log(data);
+    console.log(data.results);
     setContent(data.results);
   }
   useEffect(()=>{
@@ -20,7 +21,7 @@ function Latest() {
     <div className="Latest">
        {
          content && content.map((c)=>(
-            console.log(c)
+            <Cards key={c.id} id={c.id} />
          ))
        }
     </div> 
